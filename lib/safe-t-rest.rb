83f872b6b1d1389cd41df8765c6720895701148e
bararchy <bar.hofesh@safe-t.com>
2015-03-11 15:39:01 +0200

    got rid of exccess reused string

--- lib/safe-t-rest.rb
+++ lib/safe-t-rest.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 
 class SafeTRest
   attr_accessor :extension_id, :user_name, :password, :url, :role_id
-                
+
   def initialize(config_hash={})
     @extension_id = config_hash[:extension_id]
     @user_name = config_hash[:user_name]
@@ -12,39 +12,39 @@ class SafeTRest
   end
 
   def get_apps_process_state(guid)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', GetAppsProcessState: ['#{guid}', false]}")
+    send_request("GetAppsProcessState: ['#{guid}', false]}")
   end
 
   def get_package_file_list(guid)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', GetPackageFileList: ['#{guid}']}")
+    send_request("GetPackageFileList: ['#{guid}']}")
   end
 
   def verify_user_account
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iVerifyUserAccount: ['#{@user_name}', '#{@password}', true]}")
+    send_request("iVerifyUserAccount: ['#{@user_name}', '#{@password}', true]")
   end
 
   def get_user_history(days)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iGetHistory: ['#{@user_name}', '#{@password}', #{days}]}")
+    send_request("iGetHistory: ['#{@user_name}', '#{@password}', #{days}]")
   end
 
   def user_account_address_list
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iUserAccountAddressList: ['#{@user_name}', '#{@password}']}")
+    send_request("iUserAccountAddressList: ['#{@user_name}', '#{@password}']")
   end
 
   def get_folder_list(path)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iGetFolderList: ['#{@user_name}', '#{@password}', false, true, '#{path}', 417]}")
+    send_request("iGetFolderList: ['#{@user_name}', '#{@password}', false, true, '#{path}', 417]")
   end
 
   def safe_share_file(args)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iSafeShareFile: ['#{@user_name}', '#{@password}', '#{args[:files]}', '#{args[:recipients]}', '#{args[:sender_name]}', '#{args[:sender_address]}', '#{args[:subject]}', '#{args[:message]}', #{args[:message_encryption_level]}, #{args[:delivery_method]}, '#{args[:mobile_recipient]}', #{args[:return_receipt]}, #{args[:safe_reply]}, #{args[:max_downloads]}, #{args[:package_expiry]}, '#{args[:folder_path]}', #{args[:root_folder_id]}]}")
+    send_request("iSafeShareFile: ['#{@user_name}', '#{@password}', '#{args[:files]}', '#{args[:recipients]}', '#{args[:sender_name]}', '#{args[:sender_address]}', '#{args[:subject]}', '#{args[:message]}', #{args[:message_encryption_level]}, #{args[:delivery_method]}, '#{args[:mobile_recipient]}', #{args[:return_receipt]}, #{args[:safe_reply]}, #{args[:max_downloads]}, #{args[:package_expiry]}, '#{args[:folder_path]}', #{args[:root_folder_id]}]")
   end
 
   def file_upload(args)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iFileUpload: ['#{@user_name}', '#{@password}', '#{args[:file_base64]}', '#{args[:file_name]}', '#{args[:folder_path]}', #{args[:root_folder_id]}]}")
+    send_request("iFileUpload: ['#{@user_name}', '#{@password}', '#{args[:file_base64]}', '#{args[:file_name]}', '#{args[:folder_path]}', #{args[:root_folder_id]}]")
   end
 
   def file_download(args)
-    send_request("{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', iFileDownload: ['#{@user_name}', '#{@password}', '#{args[:file_name]}', '#{args[:folder_path]}', #{args[:root_folder_id]}]}")
+    send_request("iFileDownload: ['#{@user_name}', '#{@password}', '#{args[:file_name]}', '#{args[:folder_path]}', #{args[:root_folder_id]}]")
   end
 
   private
@@ -55,7 +55,7 @@ class SafeTRest
       url: @url,
       headers: {
         servletRequestID: 'MethodRequest',
-        BusinessLogic: request_string
+        BusinessLogic: "{Username:'', Password:'', RoleID: '#{@role_id}', ExtensionID: '#{@extension_id}', #{request_string}}"
       }
     )
   end
